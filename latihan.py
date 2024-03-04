d2bfb783d906440cc30c0bf5a683d60c312439fa
RensLYC <ngabten6@gmail.com>
2024-03-04 14:28:52 +0900

    r4

--- latihan.py
+++ latihan.py
@@ -2,27 +2,27 @@ import streamlit as st
 import pandas as pd
 #import matplotlib.pyplot as plt
 
-# st.write("coding 2024")
-# df = pd.DataFrame({
-#     'No' : [1,2,3,4],
-#     'Nama' : ['abdil','kamar','power','alok'],
-#     'Nilai' : [ 50,40,40,32]
-# })
+st.write("coding 2024")
+df = pd.DataFrame({
+    'No' : [1,2,3,4],
+    'Nama' : ['abdil','kamar','power','alok'],
+    'Nilai' : [ 50,40,40,32]
+})
 
 # df
 
-def page_1():
-    st.title("halaman 1")
-    st.write('halaman ini dipake untuk intro')
-def page_2():
-    st.title("halaman 2")
-    st.write('halaman ini dipake untuk menampilkan youtube')
-def page_3():
-    st.title("halaman 3")
-    st.write('halaman ini dipake untuk rumus')
+# def page_1():
+#     st.title("halaman 1")
+#     st.write('halaman ini dipake untuk intro')
+# def page_2():
+#     st.title("halaman 2")
+#     st.write('halaman ini dipake untuk menampilkan youtube')
+# def page_3():
+#     st.title("halaman 3")
+#     st.write('halaman ini dipake untuk rumus')
     
-PAGES = {
-    "page_1" : page_1,
-    "page_2" : page_2,
-    "page_3" : page_3
-}
+# PAGES = {
+#     "page_1" : page_1,
+#     "page_2" : page_2,
+#     "page_3" : page_3
+# }
