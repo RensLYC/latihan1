cf5fffe32e3f6189240480c8705e1c21f3c8f8f2
RensLYC <ngabten6@gmail.com>
2024-03-04 14:15:38 +0900

    r1

---
+++ latihan.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+#import matplotlib.pyplot as plt
+
+st.write("coding 2024")
+df = pd.DataFrame({
+    'No' : [1,2,3,4],
+    'Nama' : ['abdil','kamar','power','alok'],
+    'Nilai' : [ 50,40,40,32]
+})
+
+df
